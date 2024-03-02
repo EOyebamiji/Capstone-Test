a7c9994659b01d8818802442a72cb23d04d8176b
Emmanuel Oyebamiji <Emmanueloluwatosin56@gmail.com>
2024-03-02 05:50:20 +0100

    Testing project

--- eks-cluster/4-nat.tf
+++ eks-cluster/4-nat.tf
@@ -1,28 +1,5 @@
-/*
-
 resource "aws_eip" "nat" {
   domain = aws_vpc.main.id
-
-  tags = {
-    Name = "nat"
-  }
-}
-
-resource "aws_nat_gateway" "nat" {
-  allocation_id = aws_eip.nat.id
-  subnet_id     = aws_subnet.public-us-east-1a.id
-
-  tags = {
-    Name = "nat"
-  }
-
-  depends_on = [aws_internet_gateway.igw]
-}
-
-*/
-
-resource "aws_eip" "nat" {
-  vpc = true
 
   tags = {
     Name = "nat"
